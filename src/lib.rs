177a7f08ebbe4674d81f9e636729e26e25803374
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2018-02-11 17:57:00 +0100

    Add some basic error handling

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ extern crate git2;
 #[macro_use] extern crate lazy_static;
 extern crate regex;
 extern crate walkdir;
+extern crate yansi;
 
 pub mod fault;
 pub mod lint;
@@ -21,11 +22,49 @@ pub mod traverse;
 use lint::TrailingWhitespace;
 use traverse::Project;
 
-pub fn main() {
-    let project = Project::open_git_workdir().unwrap(); // TODO remove unwrap
-    let lint = TrailingWhitespace::review(project.lines());
+use failure::Error;
 
-    for fault in lint {
-        println!("{}", fault.unwrap()); // TODO remove unwrap
+pub fn main() {
+    if let Err(err) = run() {
+        handle_error(err);
     }
+}
+
+pub fn run() -> Result<(), Error> {
+    let project = Project::open_git_workdir()?;
+    let lints = vec![
+        TrailingWhitespace::review(project.lines()),
+    ];
+
+    for lint in lints {
+        for fault in lint {
+            println!("{}", fault?);
+        }
+    }
+
+    Ok(())
+}
+
+/// Handles a given error.
+///
+/// This function handles a given error. That includes:
+///
+/// 1. Printing the error message.
+/// 2. Printing the error message of every causing error, recursively.
+/// 3. Exiting the process.
+///
+/// This function is intended to be used once per application at the very top layer. It should
+/// handle every occuring error.
+pub fn handle_error(err: Error) -> ! {
+    use yansi::Paint;
+
+    // Print the error
+    eprintln!("{} {}", Paint::red("error:").bold(), err);
+
+    // Print the causing errors
+    for fail in err.causes().skip(1) {
+        eprintln!("{} {}", Paint::blue("caused by:").bold(), fail);
+    }
+
+    ::std::process::exit(1);
 }
